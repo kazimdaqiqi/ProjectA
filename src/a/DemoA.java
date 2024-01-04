d55ee3f87522ed1878a4a7abb0f204831b4208fd
kazimdaqiqi <155591206+kazimdaqiqi@users.noreply.github.com>
2024-01-03 21:37:17 -0500

    Update DemoA.java

--- src/a/DemoA.java
+++ src/a/DemoA.java
@@ -9,5 +9,11 @@ public class DemoA {
 	public static void add(int a, int b) {
 		System.out.println(a+b);
 	}
+	
 
+	// Sprint 2 automation 
+	public static void subtract(int a, int b) {
+		System.out.println(a-b);
+	}
+	
 }
