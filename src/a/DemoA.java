6abd5c79d1d969fa1cb09a7017bf7b8b5133868e
Kazim Daqiqi <Kazim Daqiqi@Mohamma.mynetworksettings.com>
2024-01-03 21:30:18 -0500

    Implemented add()

--- src/a/DemoA.java
+++ src/a/DemoA.java
@@ -4,5 +4,10 @@ public class DemoA {
 	public static void main (String args[]) {
 		System.out.println("Kazim");
 	}
+	
+	// Sprint 2 automation 
+	public static void add(int a, int b) {
+		System.out.println(a+b);
+	}
 
 }
